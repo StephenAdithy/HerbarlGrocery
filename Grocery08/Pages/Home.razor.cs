c1eb2455bafedd6ec5569828087850df2d5f457a
steve <steve@ADITHYA>
2024-04-27 16:57:14 +0530

    Add project files.

---
+++ Grocery08/Pages/Home.razor.cs
@@ -0,0 +1,19 @@
+﻿using MudBlazor;
+
+namespace Grocery08.Pages
+{
+    public partial class Home
+    {
+        bool open;
+        string drawerWidth = "400px";
+        void ToggleDrawer()
+        {
+            open = !open;
+            drawerWidth = open ? "400px" : "0px";
+        }
+        private bool arrows = true;
+        private bool enableSwipeGesture = true;
+        private bool autocycle = true;
+        private Transition transition = Transition.Slide;
+    }
+}
